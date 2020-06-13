1354b9d790e05ff94987eb48ed0ef4f6252bffb5
pdimarcodev <pablo.d.dimarco@gmail.com>
2020-06-13 18:14:29 -0300

    updated group itmes in cart

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -13,10 +13,10 @@ const CollectionPreview = ({title, items}) =>  (
             {
                 items
                 .filter((item, idx) => idx < 4 )
-                .map(({id, ...otherItemProps}) => (
-                    <CollectionItem key={id} {...otherItemProps} />
+                .map((item) => (
+                    <CollectionItem key={item.id} item={item} />
                 )) 
-            }
+            } 
  
         </div>
  
