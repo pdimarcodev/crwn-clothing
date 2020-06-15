94c83f6d9ecb015c538515630172b81a3c2deb21
pdimarcodev <pablo.d.dimarco@gmail.com>
2020-06-15 09:29:48 -0300

    added checkout page and updated cart to use selectors

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { createStructuredSelector } from 'reselect';
 
 import './App.css';
 
@@ -9,12 +10,14 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'; 
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 class App extends React.Component {
 
   unsubscribeFromAuth = null;
-
+  
   componentDidMount(){
 
     const { setCurrentUser } = this.props;
@@ -51,6 +54,7 @@ class App extends React.Component {
       <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
+      <Route exact path='/checkout' component={CheckoutPage} />
       <Route exact path='/signin' render={() => 
           this.props.currentUser ? 
           (<Redirect to="/" />) :  
@@ -61,8 +65,8 @@ class App extends React.Component {
   )};  
 }
 
-const mapStateToProps = ({user}) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
